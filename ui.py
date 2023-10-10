ca0fb46873cf1205d99a8a5e0fd299255cc5a693
Konstantin Smirnov <sasharugby@yandex.ru>
2023-10-10 19:50:06 +0300

    goodDz112

--- ui.py
+++ ui.py
@@ -5,35 +5,38 @@ from print_data import print_file
 
 
 def check_number(n):
-     while command <1 or command >5:
-            command=int(input("Ошибка,такого номера команды "
-                       "не сущуствует! Введите цифру от 1 до 5\n"       
-                      "Выберите файл: \n"
-                      "1.Добавить \n"
-                      "2.Удалить \n"
-                      "3.Изменить\n"
-                      "4.Вывод\n"
-                      "5.Выход\n"))
+    while n < 1 or n > 5:
+        n = int(input("Ошибка, такого номера команды не "
+                      "существует! Введите цифру от 1 до 5\n"
+                      "Выберите функцию:\n"
+                      "1. Добавить\n"
+                      "2. Удалить\n"
+                      "3. Изменить\n"
+                      "4. Вывод\n"
+                      "5. Выход\n"
+                      "Введите номер команды: "))
+    return n
 
 
 def start_menu():
-    command=int(input("Доброго времени суток\n"
-                      "Выберите файл: \n"
-                      "1.Добавить \n"
-                      "2.Удалить \n"
-                      "3.Изменить\n"
-                      "4.Вывод\n"
-                      "5.Выход\n"))
-   
-    
-    while command !=5:
-        if command ==1:
-             add_row()
-        elif command ==2:
-             delete_row()
-        elif command ==3:
-             change_row()
-        elif command ==4:
-             print_file()
-        
-        pass
+    command = None
+    while command != 5:
+        command = int(input("Доброго времени суток!\n"
+                            "Выберите функцию:\n"
+                            "1. Добавить\n"
+                            "2. Удалить\n"
+                            "3. Изменить\n"
+                            "4. Вывод\n"
+                            "5. Выход\n"
+                            "Введите номер команды: "))
+        command = check_number(command)
+        if command == 1:
+            add_row()
+        elif command == 2:
+            delete_row()
+        elif command == 3:
+            change_row()
+        elif command == 4:
+            print_file()
+    print("Спасибо, что воспользовались нашими услугами!\n"
+          "Всего доброго! Приходите к нам ещё :)")
